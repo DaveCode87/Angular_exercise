f49589f223ce9c350de87bfbc1b7b5b48fb56c02
Davide081987 <davidetrovato87@gmail.com>
2020-07-06 12:44:45 +0200

    list display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-exercise';
+  title = 'Contatore';
+
+  counter: number = 0;
+
+  add(): void {
+    this.counter++;
+  }
+
+  remove(): void {
+    this.counter--;
+  }
 }
